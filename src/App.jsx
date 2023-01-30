d294038c750fd50a2f3da6fe28132a3f092aa1da
Hedenfeldt <Hedenfeldt@outlook.com>
2023-01-30 11:11:50 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return <Box>hejhej</Box>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "@mui/system";
+import { themeOptions } from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={themeOptions}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
