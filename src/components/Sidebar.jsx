0df3de7b65f6bca5e9bcaddd3a8201c26493e49c
Hedenfeldt <Hedenfeldt@outlook.com>
2023-01-31 16:58:46 +0100

    can noe get playlists

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,12 @@ import { Box, Divider } from "@mui/material";
 import { Link } from "react-router-dom";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import SidebarPlaylistItem from "./SidebarPlaylistItem";
+import { useSelector } from "react-redux";
 
 export default function Sidebar() {
+  const { albumList } = useSelector((state) => state.playlist);
+  console.log(albumList);
+
   return (
     <Box
       sx={{
@@ -36,10 +40,9 @@ export default function Sidebar() {
       </Box>
 
       <Box sx={{ flex: 1, overflowY: "auto" }}>
-        <SidebarPlaylistItem />
-        <SidebarPlaylistItem />
-        <SidebarPlaylistItem />
-        <SidebarPlaylistItem />
+        {albumList.map((album) => (
+          <SidebarPlaylistItem key={album.id} id={album.id} name={album.name} />
+        ))}
       </Box>
     </Box>
   );
--- src/components/SidebarPlaylistItem.jsx
+++ src/components/SidebarPlaylistItem.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export default function SidebarPlaylistItem() {
+export default function SidebarPlaylistItem({ name, id }) {
   return (
-    <Link to="/" style={{ textDecoration: "none" }}>
+    <Link to={`/playlist/${id}`} style={{ textDecoration: "none" }}>
       <Box
         px={3}
         py={1}
         sx={{ color: "text.secondary", cursor: "pointer", fontSize: 14 }}
       >
-        Trainspotters
+        {name}
       </Box>
     </Link>
   );
